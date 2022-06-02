81c71f14f0a8c5ab28f5eb1462f70c81c6cdbb08
Michael Maggiore <michaelpmaggiore@gmail.com>
2022-06-02 16:07:37 -0700

    Delete initial hash line and added space to comments

--- Hash generator.py
+++ Hash generator.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python
 # coding: utf-8
 
 
-#Libraries to add a random number generator to implement hash generator
+# Libraries to add a random number generator to implement hash generator
 import time
 import hashlib
 import random
 
+# Arbitrary string we are hashing.
 a_string = 'poly'
 
-#Initial hashed string to initiate hashing process
-hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
-
-#Below for-loop creates 15 data values for randomly generated hashed numbers/chars
+# Below for-loop creates 15 data values for randomly generated hashed numbers/chars
 for i in range(14):
     n = random.random()
     hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()+f"{n}"
